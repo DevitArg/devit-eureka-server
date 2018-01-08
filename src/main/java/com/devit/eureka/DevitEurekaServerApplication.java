55cab60cf085018b81f1c5575c4d9b9b4fbde9e2
Lucas Godoy <godoy.lucas.e@gmail.com>
2018-01-08 16:01:48 -0300

    Initial commit

---
+++ src/main/java/com/devit/eureka/DevitEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.devit.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DevitEurekaServerApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DevitEurekaServerApplication.class, args);
+	}
+
+}
